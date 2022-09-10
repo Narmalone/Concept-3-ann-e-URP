929d613cf189977cd116d67d5139a6440541429a
Narmalone <62171923+Narmalone@users.noreply.github.com>
2022-09-10 16:38:50 +0200

    r

--- Assets/Scripts/Ressources/PlayersTitle.cs
+++ Assets/Scripts/Ressources/PlayersTitle.cs
@@ -1,32 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using System;
+using UnityEngine.UIElements;
 public class PlayersTitle : MonoBehaviour, IDataPersistence
 {
-    [Header("Titre du joueur, titre suivant � chaque fois que le joueur atteint un objectif")]
-    [SerializeField, Tooltip("Liste des diff�rents titre que le joueur obtient suivant sa progression.")] private string[] m_playersTitle;
+
+    [Header("Ui Document")]
+    [SerializeField] private UIDocument m_UiDocument;
+
+    [Header("Titre du joueur, titre suivant à chaque fois que le joueur atteint un objectif")]
+    [SerializeField, Tooltip("Liste des différents titre que le joueur obtient suivant sa progression.")] private string[] m_playersTitle;
     private int m_currentPlayerTitle;
 
-    private TextMeshProUGUI m_TitleText;
+    private Label m_playersTitleText;
 
     #region Database
     public void LoadData(GameData data)
     {
-        m_TitleText.text = data.m_currentPlayersTitle;
+        m_playersTitleText.text = data.m_currentPlayersTitle;
     }
 
     public void SaveData(GameData data)
     {
-        data.m_currentPlayersTitle = this.m_TitleText.text;
+        data.m_currentPlayersTitle = this.m_playersTitleText.text;
     }
 
     #endregion
 
     private void Awake()
     {
-        m_TitleText = GetComponent<TextMeshProUGUI>();
+        var rootElement = m_UiDocument.rootVisualElement;
+        m_playersTitleText = rootElement.Q<Label>("PlayersTitleLabel");
     }
 
     //Fonction lorsque le joueur a suffisamment d'or pour passer au titre suivant
@@ -35,7 +40,7 @@ public class PlayersTitle : MonoBehaviour, IDataPersistence
         if (m_currentPlayerTitle <= m_playersTitle.Length - 1)
         {
             m_currentPlayerTitle++;
-            m_TitleText.text = "" + m_playersTitle[m_currentPlayerTitle];
+            m_playersTitleText.text = "" + m_playersTitle[m_currentPlayerTitle];
         }
     }
 }
