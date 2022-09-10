f8d3898f4f13a9763e38d01eba81a23e3549dd67
Narmalone <62171923+Narmalone@users.noreply.github.com>
2022-09-10 16:41:08 +0200

    r

--- Assets/Scripts/Ui/City/Navigate.cs
+++ Assets/Scripts/Ui/City/Navigate.cs
@@ -1,37 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Navigate : MonoBehaviour
 {
+
+    [Header("UiDocument")]
+    [SerializeField] private UIDocument m_UiDocument;
+
     [Header("Button")]
     [SerializeField, Tooltip("Référence au bouton servant à faire apparaître/disparaître l'interface de navigation")] private Button m_navigateButton;
-    [SerializeField, Tooltip("Référence au bouton servant retourner au menu en sauvegardant")] private Button m_backButton;
-
-    [Header("Check")]
-    [SerializeField, Tooltip("Permet de savoir si le pannel est actif ou non")] private bool m_enablePannel = false;
-
-    [Header("Animations")]
-    [SerializeField, Tooltip("Permettre de trigger l'animation d'activer/désactiver le pannel par un animator")] private Animator m_EnablePannelAnimator;
+    private Button m_backButton;
 
     private void Awake()
     {
-        //Initialiser sous variable locale le bouton de navigation//
-        Button btn = m_navigateButton.GetComponent<Button>();
-        Button backButton = m_backButton.GetComponent<Button>();
+        var rootElement = m_UiDocument.rootVisualElement;
 
-        //Créer une fonction quand on click sur le bouton
-        btn.onClick.AddListener(TaskOnClick);
-        backButton.onClick.AddListener(BackToMainMenu);
+        m_backButton = rootElement.Q<Button>("B_MainMenu");
+        m_backButton.clickable.clicked += OnBackButtonClicked;
     }
 
-    private void BackToMainMenu()
+    private void OnBackButtonClicked()
     {
-
+        Debug.Log("OnBackButtonCliqued");
         //sauvegarder le jeux
         DataPersistenceManager.instance.SaveGame();
 
@@ -39,20 +33,11 @@ public class Navigate : MonoBehaviour
         SceneManager.LoadSceneAsync("MainMenu");
     }
 
-
-    //Fonction lorsque le bouton navigation est cliqué//
-    private void TaskOnClick()
+    private void OnDestroy()
     {
-        if(m_enablePannel == false)
-        {
-            m_enablePannel = true;
-            m_EnablePannelAnimator.SetTrigger("OnEnable");
-        }
-        else 
-        {
-            m_enablePannel = false;
-            m_EnablePannelAnimator.SetTrigger("OnDisable");
-        }
+        m_backButton.clickable.clicked -= OnBackButtonClicked;
     }
+
+
 
 }
