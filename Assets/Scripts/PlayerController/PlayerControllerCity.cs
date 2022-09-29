acc0c9f8152612d3d9d718427858f0708490164f
Narmalone <62171923+Narmalone@users.noreply.github.com>
2022-09-29 21:56:59 +0200

    r

--- Assets/Scripts/PlayerController/PlayerControllerCity.cs
+++ Assets/Scripts/PlayerController/PlayerControllerCity.cs
@@ -7,15 +7,45 @@ public class PlayerControllerCity : MonoBehaviour
     [SerializeField] private CharacterController charaController;
     [SerializeField] private float moveSpeed = 1;
     Vector3 moveDirection = Vector3.zero;
+    [SerializeField] private Animator m_charaAnim;
+    private bool isWalking = false;
+
+    private float inputH;
+    private float inputV;
 
     private void Awake()
     {
         charaController = GetComponent<CharacterController>();
     }
-  
+    private void Update()
+    {
+       
+        if (isWalking)
+        {
+            m_charaAnim.SetBool("isWalking" , true);
+        }
+        else
+        {
+            m_charaAnim.SetBool("isWalking", false);
+        }
+        Debug.Log(m_charaAnim);
+    }
+
     private void FixedUpdate()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        inputV = Input.GetAxis("Vertical");
+        inputH = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(inputH, 0f, inputV);
         charaController.SimpleMove(move * moveSpeed);
+
+        if (charaController.velocity.x > 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
     }
 }
